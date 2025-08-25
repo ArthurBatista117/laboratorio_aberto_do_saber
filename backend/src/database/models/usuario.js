ea84226c1c17eec5f2e8676f0d831f6f781274a7
ArthurBatista117 <167575173+ArthurBatista117@users.noreply.github.com>
2025-08-25 20:24:23 -0300

    mais um teste2

--- backend/src/database/models/usuario.js
+++ backend/src/database/models/usuario.js
@@ -1,20 +1,35 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../database');
+'use strict';
+const { Model } = require('sequelize');
 
-class Usuario extends Model {}
-
-Usuario.init({
-  nome: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, unique: true, allowNull: false },
-  senha: { type: DataTypes.STRING, allowNull: false },
-  cpf: { type: DataTypes.STRING, unique: true, allowNull: false },
-  telefone: { type: DataTypes.STRING, unique: true, allowNull: false },
-  tipo: { type: DataTypes.ENUM('admin','normal'), defaultValue: 'normal', allowNull: false }
-}, {
-  sequelize,
-  modelName: 'Usuario',
-  tableName: 'Usuarios',
-  timestamps: true
-});
-
-module.exports = Usuario;
+module.exports = (sequelize, DataTypes) => {
+  class Usuario extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  Usuario.init({
+    nome: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    senha: DataTypes.STRING,
+    cpf: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    telefone: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    tipo: {
+      type: DataTypes.ENUM('admin', 'normal'),
+      defaultValue: 'normal',
+      allowNull: false
+    }
+  }, {
+    sequelize, // << obrigatório, vem do parâmetro da função
+    modelName: 'Usuario'
+  });
+  return Usuario;
+};
