6688b9a5123ce0a44921dba225d33b1bfcc277e7
ArthurBatista117 <167575173+ArthurBatista117@users.noreply.github.com>
2025-08-25 19:41:26 -0300

    Teste de config

--- backend/src/database/config/config.cjs
+++ backend/src/database/config/config.cjs
@@ -1,28 +1,12 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DATABASE_URL,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT,
-    dialectOptions: {}
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
   },
-  "test": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DATABASE_URL,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT,
-    dialectOptions: {}
-  },
-  "production": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DATABASE_URL,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT,
-    dialectOptions: {}
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
   }
-}
+};
