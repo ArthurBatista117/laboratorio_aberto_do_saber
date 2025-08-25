89e2ad9ab429b29267198ae7874b5a50eebd5b97
ArthurBatista117 <167575173+ArthurBatista117@users.noreply.github.com>
2025-08-25 20:32:43 -0300

    agora vai não é possivel

--- backend/src/database/models/usuario.js
+++ backend/src/database/models/usuario.js
@@ -4,32 +4,24 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Usuario extends Model {
     static associate(models) {
-      // define association here
+      // define associações aqui
     }
   }
+
   Usuario.init({
     nome: DataTypes.STRING,
-    email: {
-      type: DataTypes.STRING,
-      unique: true
-    },
+    email: { type: DataTypes.STRING, unique: true },
     senha: DataTypes.STRING,
-    cpf: {
-      type: DataTypes.STRING,
-      unique: true
-    },
-    telefone: {
-      type: DataTypes.STRING,
-      unique: true
-    },
-    tipo: {
-      type: DataTypes.ENUM('admin', 'normal'),
-      defaultValue: 'normal',
-      allowNull: false
-    }
+    cpf: { type: DataTypes.STRING, unique: true },
+    telefone: { type: DataTypes.STRING, unique: true },
+    tipo: { type: DataTypes.ENUM('admin', 'normal'), defaultValue: 'normal', allowNull: false },
   }, {
-    sequelize, // << obrigatório, vem do parâmetro da função
-    modelName: 'Usuario'
+    sequelize,
+    modelName: 'Usuario',
+    tableName: 'usuarios', // <-- aqui você força o nome da tabela
+    timestamps: true,      // createdAt e updatedAt
   });
+
   return Usuario;
 };
+
