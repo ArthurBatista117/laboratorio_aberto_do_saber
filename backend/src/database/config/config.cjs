bf6c3b701eba4375d7e2c448714338e10e544773
ArthurBatista117 <167575173+ArthurBatista117@users.noreply.github.com>
2025-08-25 19:53:24 -0300

    Revert "Teste de config"
    This reverts commit 6688b9a5123ce0a44921dba225d33b1bfcc277e7.

--- backend/src/database/config/config.cjs
+++ backend/src/database/config/config.cjs
@@ -1,12 +1,28 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres',
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DATABASE_URL,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    dialectOptions: {}
   },
-  production: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres',
+  "test": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DATABASE_URL,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    dialectOptions: {}
+  },
+  "production": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DATABASE_URL,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    dialectOptions: {}
   }
-};
+}
