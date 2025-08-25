937cedb48e449b02eb14cb28ca3d9b8077431109
ArthurBatista117 <167575173+ArthurBatista117@users.noreply.github.com>
2025-08-25 19:04:34 -0300

    tentando subir o banco corretamente

--- backend/src/database/config/config.cjs
+++ backend/src/database/config/config.cjs
@@ -6,20 +6,23 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DATABASE_URL,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "dialect": process.env.DB_DIALECT,
+    dialectOptions: {}
   },
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DATABASE_URL,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "dialect": process.env.DB_DIALECT,
+    dialectOptions: {}
   },
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DATABASE_URL,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "dialect": process.env.DB_DIALECT,
+    dialectOptions: {}
   }
 }
