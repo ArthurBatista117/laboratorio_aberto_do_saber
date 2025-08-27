ba4bfcc9d7b57f4f1db361f9e0f8a4416078a86c
ArthurBatista117 <167575173+ArthurBatista117@users.noreply.github.com>
2025-08-27 19:59:33 -0300

    corrigindo erro do hash

--- backend/src/middlewares/hash/hashUser.js
+++ backend/src/middlewares/hash/hashUser.js
@@ -3,12 +3,12 @@ const bcrypt = require('bcrypt');
 const hashUser = {
     encripitar: async (senha) => {
         const salt = 20;
-        const hash = bcrypt.hash(senha, salt);
+        const hash = await bcrypt.hash(senha, salt);
         return hash;
     },
 
     descripitar: async (senha, hash) => {
-        const promise = bcrypt.compare(senha, hash);
+        const promise = await bcrypt.compare(senha, hash);
         return promise;
     }
 
